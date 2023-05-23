cec9fa89ff05c47c1fc1f310984c6f8197a52bda
HyunTaekO <htoh55@naver.com>
2023-05-24 04:23:28 +0900

    solved.ac

---
+++ SolvedAC/BOJ/구현/구현_2439(별찍기-2).swift
@@ -0,0 +1,26 @@
+//
+//  구현_2439(별찍기-2).swift
+//  SolvedAC
+//
+//  Created by 오현택 on 2023/05/24.
+//
+
+import Foundation
+
+//let N = Int(readLine()!)!
+//var space = ""
+//
+//for i in 0..<N {
+//    space = {
+//        for _ in 0..<(N-i) - 1 {
+//            space += " "
+//        }
+//        return space
+//    }()
+//    print(space, terminator: "")
+//    for _ in 0...i {
+//        print("*", terminator: "")
+//    }
+//    print("")
+//    space = ""
+//}
